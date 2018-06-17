845b632cd4078fa5eccb3542a9175540a13c3546
mjbae <0131mj@gmail.com>
2018-06-18 00:27:16 +0900

    카운터 예제 작성중

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import Counter from './component/Counter'
+import Counter from '../components/Counter'
 
 class App extends Component{
     render(){
         return(
             <div>
-                Counter
+                <Counter/>
             </div>
         )
     }
---
+++ redux-counter/src/reducers/index.js
@@ -0,0 +1,7 @@
+import * as types from '../actions/ActionTypes';
+
+// 초기 상태를 정의합니다
+const initialState = {
+    color: 'black',
+    number: 0
+};
