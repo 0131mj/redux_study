8e6ba0877790b79673ec540720e8c89aabb047e9
mjbae <0131mj@gmail.com>
2018-08-09 08:29:25 +0900

    멀티카운터 작성 (todo : 소스 원리 파악 및 버그 수정)

--- redux-counter/src/components/App.js
+++ redux-counter/src/components/App.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
-import CounterContainer from '../containers/CounterContainer'
+import Buttons from '../components/Buttons'
+import CounterContainer from '../containers/CounterListContainer'
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import { getRandomColor } from "../utils";
 
 class App extends Component {
     render() {
+        const { onCreate, onRemove } = this.props;
         return (
-            <div>
+            <div className="App">
+                <Buttons
+                    onCreate={onCreate}
+                    onRemove={onRemove}
+                />
                 <CounterContainer/>
             </div>
         );
     }
 }
 
-export default App;
+const mapToDispatch = (dispatch) => ({
+    onCreate: () => dispatch(actions.create(getRandomColor())),
+    onRemove:(index) => dispatch(actions.remove(index))
+});
+
+export default connect(null, mapToDispatch)(App);
